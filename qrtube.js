f6c518e9bc29dd8029c4904a271c79c89d6fc0f5
Leo Sun <leo.tcsun@gmail.com>
2014-06-09 17:29:18 -0400

    added bootstrap and refactored js code

--- qrtube.js
+++ qrtube.js
@@ -1,21 +1,30 @@
-var tabUrl;
-var qrcode;
 
-chrome.tabs.getSelected(null,function(tab) {
-    tabUrl = tab.url;
-    $("#url-input-box").val(tabUrl);
-    qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: tabUrl,
-        width: 128,
-        height: 128
+function main() {
+    var tabUrl;
+    var qrcode;
+    var contentInputBox = "#content-input-box";
+    var generateButton = "#generate-button";
+
+    document.addEventListener('DOMContentLoaded', function() {
+        chrome.tabs.getSelected(null,function(tab) {
+            tabUrl = tab.url;
+            $(contentInputBox).val(tabUrl);
+            qrcode = new QRCode($("#qrcode")[0], {
+                text: tabUrl,
+                width: 128,
+                height: 128
+            });
+        });
+
+        $(generateButton)[0].addEventListener('click', function() {
+            var content = $(contentInputBox).val();
+            qrcode.clear();
+            qrcode.makeCode(content);
+        });
+
+        $(contentInputBox).on('mouseup', function() { $(this)[0].select(); });
     });
-});
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById("generate-btn").addEventListener('click', function(){
-    var content = $("#url-input-box").val();
-    qrcode.clear();
-    qrcode.makeCode(content);
-  });
-});
+};
+main();
